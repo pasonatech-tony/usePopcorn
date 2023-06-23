75b961a460f917348f0a405ee21a658912fd5636
joanskenderi <joanskenderi.dev@gmail.com>
2023-06-24 00:00:11 +0200

    section 11 done

---
+++ src/components/WatchedMoviesList.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import WatchedMovie from "./WatchedMovie";
+
+const WatchedMoviesList = ({ watched }) => {
+  return (
+    <ul className="list">
+      {watched.map((movie) => (
+        <WatchedMovie movie={movie} key={movie.imdbID} />
+      ))}
+    </ul>
+  );
+};
+
+export default WatchedMoviesList;
